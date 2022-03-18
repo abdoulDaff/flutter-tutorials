16b57a27c65a58e3a81c0fb5aa6c05e8cd801600
AbdoulDaff <abdoulniallydaff@gmail.com>
2022-03-19 00:05:54 +0100

    add sideBar

--- 001-tik_tok_ui/tik_tok_ui/phase2/lib/home.dart
+++ 001-tik_tok_ui/tik_tok_ui/phase2/lib/home.dart
@@ -1,33 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:phase2/widgets/actions_toolbar.dart';
-import 'package:phase2/widgets/bottom_toolbar.dart';
-import 'package:phase2/widgets/video_description.dart';
 
+/// Empty page widget fo developers that want to follow along with the tutorial
+/// on breaking down and implementing the Tik Tok UI (BLOG_LINK)
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
-  Widget get topSection => Container(height: 100.0, color: Colors.yellow[300]);
-
-  Widget get middleSection => Expanded(
-      child: Row(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[VideoDescription(), ActionsToolbar()]));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(child: Container()),
       body: Column(
         children: <Widget>[
           // Top section
-          topSection,
+          Container(
+            height: 100.0,
+            padding: const EdgeInsets.only(bottom: 15.0),
+            color: Colors.yellow[300],
+          ),
 
           // Middle expanded
-          middleSection,
+          Expanded(
+              child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                Expanded(child: Container(color: Colors.green[300])),
+                Container(
+                  width: 100.0,
+                  color: Colors.red[300],
+                )
+              ])),
 
           // Bottom Section
-          BottomToolbar(),
+          Container(height: 80.0, color: Colors.blue[300]),
         ],
       ),
     );
