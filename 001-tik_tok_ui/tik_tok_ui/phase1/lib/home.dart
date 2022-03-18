06b1366d9d42719cc306f385578656bdfe1ecd15
AbdoulDaff <abdoulniallydaff@gmail.com>
2022-03-18 23:53:31 +0100

    delete sidebar

--- 001-tik_tok_ui/tik_tok_ui/phase1/lib/home.dart
+++ 001-tik_tok_ui/tik_tok_ui/phase1/lib/home.dart
@@ -5,41 +5,28 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
-  Widget get topSection => Container(
-        height: 100.0,
-        padding: const EdgeInsets.only(bottom: 15.0),
-        color: Colors.yellow[300],
-      );
-
-  Widget get middleSection => Expanded(
-          child: Row(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-            Expanded(child: Container(color: Colors.green[300])),
-            Container(
-              width: 100.0,
-              color: Colors.red[300],
-            )
-          ]));
-
-  Widget get bottomSection => Container(height: 80.0, color: Colors.blue[300]);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          // Top section
-          topSection,
-
-          // Middle expanded
-          middleSection,
-
-          // Bottom Section
-          bottomSection,
-        ],
-      ),
-    );
+        body: Column(
+      children: <Widget>[
+        // Top Section
+        Container(
+          height: 100,
+          color: Colors.yellow[300],
+        ),
+        // Middle Section
+        Expanded(
+          child: Container(
+            color: Colors.green[300],
+          ),
+        ),
+        // Bottom Section
+        Container(
+          height: 80.0,
+          color: Colors.blue[300],
+        ),
+      ],
+    ));
   }
 }
