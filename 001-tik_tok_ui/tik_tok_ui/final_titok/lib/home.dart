b5c199a575e71910425627b0345dd57e35965bbb
AbdoulDaff <abdoulniallydaff@gmail.com>
2022-03-19 01:14:43 +0100

    Change actionsToolbar and add 5 sections

--- 001-tik_tok_ui/tik_tok_ui/final_titok/lib/home.dart
+++ 001-tik_tok_ui/tik_tok_ui/final_titok/lib/home.dart
@@ -11,27 +11,38 @@ class Home extends StatelessWidget {
 
   Widget get videoDescription => Expanded(
         child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                  height: 10.0,
-                  color: Colors.green[300],
-                  margin: const EdgeInsets.only(top: 10)),
-              Container(
-                  height: 10.0,
-                  color: Colors.green[300],
-                  margin: const EdgeInsets.only(top: 10)),
-              Container(
-                  height: 10.0,
-                  color: Colors.green[300],
-                  margin: const EdgeInsets.only(top: 10))
-            ]),
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                height: 10.0,
+                color: Colors.green[300],
+                margin: const EdgeInsets.only(top: 10)),
+            Container(
+                height: 10.0,
+                color: Colors.green[300],
+                margin: const EdgeInsets.only(top: 10)),
+            Container(
+                height: 10.0,
+                color: Colors.green[300],
+                margin: const EdgeInsets.only(top: 10))
+          ],
+        ),
       );
 
   Widget get actionsToolbar => Container(
         width: 100.0,
         color: Colors.red[300],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: List<Widget>.generate(
+              5,
+              (index) => Container(
+                  width: 60,
+                  height: 60,
+                  color: Colors.blue[300],
+                  margin: const EdgeInsets.only(top: 20.0))),
+        ),
       );
 
   Widget get middleSection => Expanded(
